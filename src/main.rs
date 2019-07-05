c0c5f656c1a67b2842252172278a6e21df8f8a3c
Thanh Tu <tu@novalex.vn>
2019-07-05 14:27:22 +0700

    add macro

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 use hello_world::*;
 
+macro_rules! calculate {
+    (eval $e:expr) => {{
+        {
+            let val: usize = $e; // Force types to be integers
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+}
+
 fn main() {
     let mut dolly: Sheep = Animal::new("Dolly");
     dolly.talk();
     dolly.shear();
     dolly.talk();
     dolly.shear();
+
+    calculate! {
+        eval 1 + 2 // hehehe `eval` is _not_ a Rust keyword!
+    }
+
+    calculate! {
+        eval (1 + 2) * (3 / 4)
+    }
 }
